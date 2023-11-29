e5573bb3435cd172a588737f64e7847e31e902bd
Sadiq6604 <saddiqumukhtar67@gmail.com>
2023-11-29 15:12:48 -0800

    redis

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,15 @@
+import redis from 'redis';
+const client = redis.createClient();
+
+client.on("error", (error) => {
+  if (error) console.log(`Redis client not connected to the server: ${error}`)
+}).on('ready', () => {
+    console.log('Redis client connected to the server');
+});
+
+
+function setNewSchool(schoolName, value) {
+    client.set(schoolName, value, (error, reply) => {
+        redis.print(`Reply: ${reply}`);
+    });
+}
